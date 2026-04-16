f7073ad9e1869d8b5c7b8252fc9f930b1c13b5e5
Esmi415 <esveralinares@s.sfusd.edu>
2026-04-16 11:57:06 -0700

    almost done on 21]

--- Scripts/end_flag.gd
+++ Scripts/end_flag.gd
@@ -2,9 +2,8 @@ extends Area2D
 
 @export var scene_to_load : PackedScene
 
-func _on_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	if not body.is_in_group("Player")
+func _on_body_entered(body):
+	if not body.is.in_group("Player"):
 		return
-		
-		get_tree().change_scene_to_packed(scene_to_load)
-		
+	
+	get_tree().change_scene_to_packed(scene_to_load)
